659fffddeda4385bd01e2a56d8a8995596172d29
lalithabhupalam <43906167+lalithabhupalam@users.noreply.github.com>
2024-03-26 23:52:47 -0700

    Day 1

---
+++ Band Name generator.py
@@ -0,0 +1,12 @@
+#Band name Generator - Day 1 Python bootcamp 
+#This program creates a band name based on your city and pet names 
+#Beginner
+
+print("Welcome to the Band Name Generator.")
+
+#inputting city and pet name
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+#printing the brand name
+print("Your band name could be " + street + " " + pet)
